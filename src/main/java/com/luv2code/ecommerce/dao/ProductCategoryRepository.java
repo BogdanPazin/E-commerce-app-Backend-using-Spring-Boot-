af3056e07d64b348a7d855e95d956f5dc37aa5af
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-17 14:31:02 +0200

    Update ProductCategoryRepository.java

--- src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-// PRVI PARAMETAR ZA ANOTACIJU CE BITI NAZIV JSON UNOSA, A DRUGI PARAMETAR OZNACAVA PUTANJU /api/product-category
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 @CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
