082fa39d182e8d8f142a619c9aa3238fef541b41
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-17 14:30:52 +0200

    Update CustomerRepository.java

--- src/main/java/com/luv2code/ecommerce/dao/CustomerRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// CUSTOMER JE ENTITY JER CUSTOMER IMA KOLEKCIJU ORDER-A, KADA SE OSTVARI KUPOVINA, UZIMAMO CUSTOMER-A, SKLAPAM GA I CUVAM GA PREKO OVOG REPOSITORY-A
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
